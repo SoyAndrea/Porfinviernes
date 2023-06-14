8e520621d112baa925d38a58dccbb916a62f83a4
juanchiem <edwardsmolina@gmail.com>
2023-06-14 15:59:20 -0300

    agregué otros graficos

--- pfvR_2023_06_23.R
+++ pfvR_2023_06_23.R
@@ -19,9 +19,16 @@ puntos <-
 puntos_soil <- puntos %>% fetchSoilGrids()
 
 # Grafico
-# https://ncss-tech.github.io/AQP/aqp/aqp-intro.html#1_Introduction
-pal <- colorRampPalette(colors = c("navajowhite", "navajowhite4"))
-plotSPC(puntos_soil, color = 'socQ50', col.palette = pal(10))
+pal_soil <- colorRampPalette(colors = c("navajowhite", "navajowhite4"))
+pal_ag <- colorRampPalette(colors = c("lightblue", "blue"))
+pal_N <- colorRampPalette(colors = c("orange", "green"))
+
+
+plotSPC(puntos_soil, color = 'socQ50', col.palette = pal_soil(10))
+plotSPC(puntos_soil, color = 'clayQ50', col.palette = pal_soil(10))
+plotSPC(puntos_soil, color = 'nitrogenQ50', col.palette = pal_N(10))
+plotSPC(puntos_soil, color = 'phh2oQ50', col.palette = pal_ag(10))
+
 
 # Final 
 puntos_profiles <- left_join(
