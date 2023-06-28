6f710ebb863b9dd42f91fe9cc9d550d265ac4f4b
juanchiem <edwardsmolina@gmail.com>
2023-06-28 10:09:28 -0300

    agregu normas de comercializacion

--- R-Tips/pfvR_2023_06_30.R
+++ R-Tips/pfvR_2023_06_30.R
@@ -2,6 +2,7 @@
 # Cada cultivo de grano tiene su humedad standard de recibo, por ej. el girasol se recibe a 11% de humedad.
 # Ademas, el girasol puede obtener bonificaciones por el contenido de aceite de los granos, 
 # esto es: para valores superiores a 42% se bonifica a razón de 2% mas de rendimiento por cada punto %.
+# http://www.agro.unc.edu.ar/~wpweb/cereales/wp-content/uploads/sites/31/2018/07/TABLA-TIPIFICACION-Calidad-Comercial-de-granos.pdf
 # Al presentar resultados de ensayos de girasol es común ver el rendimiento ajustado a 11% y sobre este, 
 # se aplica la bonificación correspondiente según % de aceite. 
 # Al tratarse de un cálculo de rutina, nos conviene tener a mano funciones que nos faciliten los cálculos en algun script.
